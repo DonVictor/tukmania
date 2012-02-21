40f50838fa3cf3ecb0180a6dd1b742c024d50769
donvictor <victor.palma.melo@gmail.com>
2012-02-21 11:23:27 -0300

    al changes

---
+++ app/cache/dev/twig/11/9d/5c8e1d3174850365434ecd5a7ce6.php
@@ -0,0 +1,44 @@
+<?php
+
+/* TukMainBundle:Default:index.html.twig */
+class __TwigTemplate_119d5c8e1d3174850365434ecd5a7ce6 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->getParent($context)->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_content($context, array $blocks = array())
+    {
+        // line 4
+        echo "\t";
+        echo twig_escape_filter($this->env, $this->getContext($context, "name"), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "TukMainBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+}
---
+++ app/cache/dev/twig/8d/dd/815ac5cf53a677432d092d0de3b6.php
@@ -0,0 +1,82 @@
+<?php
+
+/* ::layout.html.twig */
+class __TwigTemplate_8ddd815ac5cf53a677432d092d0de3b6 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'stylesheets' => array($this, 'block_stylesheets'),
+            'content' => array($this, 'block_content'),
+            'javascripts' => array($this, 'block_javascripts'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
+        <title>";
+        // line 5
+        $this->displayBlock('title', $context, $blocks);
+        echo "</title>
+        ";
+        // line 6
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 7
+        echo "        <link rel=\"shortcut icon\" href=\"";
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("favicon.ico"), "html", null, true);
+        echo "\" />
+    </head>
+    <body>
+        ";
+        // line 10
+        $this->displayBlock('content', $context, $blocks);
+        // line 11
+        echo "        ";
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 13
+        echo "    </body>
+</html>
+";
+    }
+
+    // line 5
+    public function block_title($context, array $blocks = array())
+    {
+        echo "Welcome!";
+    }
+
+    // line 6
+    public function block_stylesheets($context, array $blocks = array())
+    {
+    }
+
+    // line 10
+    public function block_content($context, array $blocks = array())
+    {
+    }
+
+    // line 11
+    public function block_javascripts($context, array $blocks = array())
+    {
+        // line 12
+        echo "        ";
+    }
+
+    public function getTemplateName()
+    {
+        return "::layout.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+}
